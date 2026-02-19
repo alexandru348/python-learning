57e8ca5eeafc28192498b779dfcbe9ac99206c32
Lazar Alexandru <alexandrulazar650@gmail.com>
2026-02-19 19:50:07 +0200

    Add input, int conversion and print example

---
+++ 01_basics/01_input_and_print.py
@@ -0,0 +1,9 @@
+# 01_input_and_print.py
+
+# Examples of input() and print()
+
+name = input("Name: ") # read value (string)
+age = int(input("Age: ")) # read value and convert to int
+print("Hello,", name) # display output
+print("Next year you will be", age + 1) # display output 
+
